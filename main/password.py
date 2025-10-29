b594d0126f010903d22af3a473492b6ec4d05388
Marcelino Escalante Martin Omar <147559007+ZDAII@users.noreply.github.com>
2025-10-29 15:01:51 -0700

    Crear password.py

---
+++ main/password.py
@@ -0,0 +1,4 @@
+WIFI_SSID = "" # Nombre de tu red
+WIFI_PASSWORD = "" # Contraseña de tu red (dejar vacio de no requerir)
+MQTT_BROKER = "mqtt.flespi.io"
+FLESPI_TOKEN = "QVd7cVAK1r8MGhk4sEajgBxM9npKejVxcPeg4nUY8b144bMDUTZHHHdTB4tw9dPh" # Agregar API Key de Flepsi
